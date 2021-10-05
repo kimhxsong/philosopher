eb4003b9e438465ef8e47da2c038bd285f257069
Hyeonsong Kim <hyeonsok@c10r6s14.42seoul.kr>
2021-10-05 21:29:30 +0900

    Add parser
    check Are vectors valid integer formats
    parse argument to t_shared variable

---
+++ ft_iterargv.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterargv.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 02:25:02 by hyeonsok          #+#    #+#             */
+/*   Updated: 2021/10/06 03:04:14 by hyeonsok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+/*
+** ft_iterargv()
+** Iterate over argument vetors as argument to is_functions()
+*/
+int	ft_iterargv(int ac, char *av[], int (*isfptr(char *)))
+{
+	int	i;
+
+	i = 1;
+	while (isfptr(av[i]) == 1)
+		i++;
+	if (i != ac)
+		return (0);
+	return (1);
+}
