d121bbb024165a2a7e04b6867778336effd322f5
hyeonsok <hyeonsok@student.42seoul.kr>
2021-11-23 04:50:20 +0900

    Fix and Rename %error.c

--- util/ft_error.c
+++ util/ft_error.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_perror.c                                        :+:      :+:    :+:   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/22 21:32:28 by hyeonsok          #+#    #+#             */
-/*   Updated: 2021/11/22 21:50:51 by hyeonsok         ###   ########.fr       */
+/*   Created: 2021/11/22 21:16:24 by hyeonsok          #+#    #+#             */
+/*   Updated: 2021/11/23 04:48:41 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo_error.h"
+#include "utilx.h"
 
-void	ft_perror(const char *s, const char *strset[], int i)
+void	ft_error(const char *s, const char *strset[], int i)
 {
 	write(2, s, ft_strlen(s));
 	write(2, ": ", 2);
-	write(2, strset[i], ft_strlen(strset[i]));
+	write(2, &strset[i], ft_strlen(strset[i]));
 }
