2366ef2adcf6b07969352427f915f5306f500131
hyeonsok <hyeonsok@student.42seoul.kr>
2021-11-23 05:52:53 +0900

    Refactor parse

--- src/main.c
+++ src/main.c
@@ -6,24 +6,11 @@
 /*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 17:35:10 by hyeonsok          #+#    #+#             */
-/*   Updated: 2021/11/22 21:16:09 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2021/11/23 05:52:38 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-/* will be removed */
-#include <string.h>
-
-
-static void	init_private(t_args *args, t_shared *s, int i);
-static int	init_args(t_args **args, t_shared *s);
-int	simul(t_shared *s);
-
-void print_glstrarr(int errnum)
-{
-	write(2, g_msg[errnum], ft_strlen(g_msg[errnum]));
-	return (errnum);
-}
 
 int	main(int argc, char *argv[])
 {
--- src/parse.c
+++ src/parse.c
@@ -6,60 +6,61 @@
 /*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 20:59:09 by hyeonsok          #+#    #+#             */
-/*   Updated: 2021/11/22 21:14:17 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2021/11/23 05:52:31 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <string.h>
 
-/*
-** init_shared()
-** Initialize t_shared struct with argument vectors
-*/
-static int	init_shared(int ac, char *av[], t_shared *shared);
+static int	validate_args(int ac, char *av[], int *errnum);
+static int	init_info(t_info *info, int ac, char *av[], int *errnum);
+static void	assign_info(t_info *info, int ac, char *av[]);
+static void	validate_info(t_info *info, int *errnum);
 
-/*
-** parse()
-** Check if vectors are valid integer formats
-** Parse argument to t_shared variable
-*/
-int	parse(t_shared *shared, int ac, char *av[])
+t_shared	*parse(t_shared *s, int ac, char *av[])
 {
-	int	idx;
+	int	errnum;
 
-	if (ac != 5 && ac != 6)
-		return (1);
-	else if (ft_iterargv(ac, av, ft_isintf) == FAIL)
-		return (2);
-	else if (init_shared(ac, av, shared) == FAIL)
-		return (3);
-	else
-		return (0);
+	errnum = 0;
+	memset(s, 1, sizeof(t_shared));
+	if (validate_args(ac, av, &errnum) || init_info(&s->info, ac, av, &errnum))
+	{
+		parse_error(errnum - 1);
+		return (NULL);
+	}
+	return (s);
 }
 
-static int	init_shared(int ac, char *av[], t_shared *shared)
+static int	validat_args(int ac, char *av[], int *errnum)
 {
-	int	res;
-
-	shared->finish = 0;
-	shared->time.current = 0;
-	shared->info.max_eat_number = -1;
-	shared->info.number_of_philo = atoi(av[1]);
-	if (shared->info.number_of_philo > PTHREAD_STACK_MIN / 2)
-		return (FAIL);
-	shared->info.time_to_die = atoi(av[2]);
-	shared->info.time_of_sleeping = atoi(av[3]);
-	shared->info.time_of_eating = atoi(av[4]);
-	if (av[5] != NULL)
-		shared->info.max_eat_number = atoi(av[5]);
-	res = shared->info.number_of_philo <= 0 \
-		|| shared->info.time_to_die < 20 \
-		|| shared->info.time_of_sleeping < 20 \
-		|| shared->info.time_of_eating < 20 \
-		|| shared->info.max_eat_number <= 0;
-	if (res != 0)
-		return (FAIL);
-	res = shared->info.time_of_eating - shared->info.time_of_sleeping;
-	return (SUCCESS);
+	if (ac < 5 || ac > 6)
+		*errnum = 1;
+	else if (ft_strarriter(ac, av, ft_isintf) == -1)
+		*errnum = 2;
+	return (*errnum);
 }
 
+static int	init_info(t_info *info, int ac, char *av[], int *errnum)
+{
+	assign_info(info, ac, av);
+	validate_info(info, errnum);
+	return (*errnum);
+}
+
+static void	assign_info(t_info *info, int ac, char *av[])
+{
+	info->number_of_philo = ft_atoi(av[1]);
+	info->time_to_die = ft_atoi(av[2]);
+	info->time_of_sleeping = ft_atoi(av[3]);
+	info->time_of_eating = ft_atoi(av[4]);
+	info->max_eat_number = ft_ternaryint(ac == 6, ft_atoi(av[5]), -1);
+}
+
+static void	validate_info(t_info *info, int *errnum)
+{
+	if ((info->number_of_philo <= 0) || (info->max_eat_number <= 0)
+		|| (info->time_to_die < 0) || (info->time_of_eating < 0)
+		|| (info->time_of_sleeping < 0))
+		*errnum = 3;
+}
